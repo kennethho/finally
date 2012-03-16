0f75e35c7f8409c6b5c46cc1973bd9776218cc36
Kenneth Ho <ken@fsfoundry.org>
2012-03-16 19:47:40 +0800

    Added MIT license and inclusion guard.

--- finally.hpp
+++ finally.hpp
@@ -1,3 +1,29 @@
+#ifndef FINALLY_HPP
+#define FINALLY_HPP
+
+/*
+Copyright (c) 2011 Kenneth Ho https://github.com/kennethho
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include <functional>
 #include <memory>
 
@@ -287,3 +313,5 @@ namespace detail
   }
 
 } // namespace detail
+
+#endif // FINALLY_HPP
